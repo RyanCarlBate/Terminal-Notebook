69517e8758eb36042d59295f2b7226bccbc65b23
Ryan Carl Bate <ryancarl.s.bate@gmail.com>
2025-09-30 20:13:30 +0800

    Added choice input

--- Terminal Notebook.cpp
+++ Terminal Notebook.cpp
@@ -17,18 +17,26 @@ int main()
     //cout << "1. Create a new note" << endl;//
     //cout << "2. View existing notes" << endl;//
     //cout << "3. Exit" << endl;//
-    cout << "Put the title of your file: ";
-    getline(cin, fileName);
-    fileName += ".txt";
-    notesStorage.open(fileName);
-    if (notesStorage.is_open()) 
+    cout << "You're current notes: " << notes << endl;
+    cout << "Enter your choice: ";
+    cin >> choices;
+    cin.ignore();
+    if (choices == 1 && notes == 0)
     {
-        cout << "File created successfully!" << endl;
-    } 
-    else
-    {
-        cout << "Error creating file!" << endl;
-        return 1;
+    //cout << "Put the title of your file: ";
+    //getline(cin, fileName);
+    //fileName += ".txt";
+    //notes++;
+    //notesStorage.open(fileName);
+    //if (notesStorage.is_open()) 
+    //{
+        //cout << "File created successfully!" << endl;
+    //} 
+    //else
+    //{
+        //cout << "Error creating file!" << endl;
+        //return 1;
+    //}
     }
 
 }
