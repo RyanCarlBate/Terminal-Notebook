98579920b52857e5a19163d5a84ece5a8af92967
Ryan Carl Bate <ryancarl.s.bate@gmail.com>
2025-10-01 21:17:52 +0800

    Added experimental code to create a file on a specific path.

--- Terminal Notebook.cpp
+++ Terminal Notebook.cpp
@@ -9,34 +9,39 @@ int main()
 {
     string fileName;
     ofstream notesStorage(fileName);
-    int choices;
-    int notes;
-    string note;
-
+    int choices, notes, noteChecker;
+    string note,filePath;
     //cout << "Welcome to Terminal Notebook!" << endl;//
     //cout << "1. Create a new note" << endl;//
     //cout << "2. View existing notes" << endl;//
     //cout << "3. Exit" << endl;//
+    filePath = R"(C:\Users\Admin\Desktop\)";
+    ofstream output_file(filePath);
     cout << "You're current notes: " << notes << endl;
     cout << "Enter your choice: ";
     cin >> choices;
     cin.ignore();
     if (choices == 1 && notes == 0)
     {
-    //cout << "Put the title of your file: ";
-    //getline(cin, fileName);
-    //fileName += ".txt";
-    //notes++;
-    //notesStorage.open(fileName);
-    //if (notesStorage.is_open()) 
-    //{
-        //cout << "File created successfully!" << endl;
-    //} 
-    //else
-    //{
-        //cout << "Error creating file!" << endl;
-        //return 1;
-    //}
+        cout << "Put the title of your file: ";
+        getline(cin, fileName);
+        fileName += ".txt";
+        notes++;
+        notesStorage.open(fileName);
+        if (notesStorage.is_open()) 
+        {
+            cout << "File created successfully!" << endl;
+        } 
+        else
+        {
+            cout << "Error creating file!" << endl;
+        }
+
+    }
+    else if (choices == 1 && notes > 0)
+    {
+        cout << "You already have a note. Please view existing notes." << endl;
     }
 
+    return 0;
 }
